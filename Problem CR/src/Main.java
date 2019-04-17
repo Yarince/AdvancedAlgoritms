1309cf57c617d0a274afcd6231ba4a9533269c8f
Yarince Martis <Yarince.Martis@gmail.com>
2019-04-18 00:39:40 +0200

    Submission 2

--- Problem CR/src/Main.java
+++ Problem CR/src/Main.java
@@ -1,15 +1,13 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
     static final StringBuilder RESULT = new StringBuilder();
-    private static ArrayList<int[]> EDGES = new ArrayList<>();
-    //    private static ArrayList<Pair> EDGES = new ArrayList<>();
-//    private static int[] moleculeArray;
-    private static HashMap<Integer, ArrayList<Triple>> EveryTriple = new HashMap<>();
+    private static HashSet<int[]> POSITIVE_EDGES = new HashSet<>();
+    private static int[] moleculeArray;
 
     public static void main(String[] args) {
         process(new Scanner(System.in));
@@ -27,38 +25,29 @@ public class Main {
         int molecules = in.nextInt();
         int positiveEdges = in.nextInt();
 
-//        moleculeArray = new int[molecules];
-//        for (int i = 1; i <= molecules; i++) {
-//            moleculeArray[i - 1] = i;
-//        }
-
-        if (!EveryTriple.containsKey(molecules-1)) {
-            int[] tempArray = new int[molecules];
-            for (int j = 1; j <= molecules; j++) {
-                tempArray[j - 1] = j;
-            }
-            EveryTriple.put(molecules-1, getTriples(tempArray, new Triple(), 0, 0));
+        moleculeArray = new int[molecules];
+        for (int i = 1; i <= molecules; i++) {
+            moleculeArray[i - 1] = i;
         }
 
-        EDGES = new ArrayList<>();
+        POSITIVE_EDGES = new HashSet<>();
         for (int i = 0; i < positiveEdges; i++) {
-            EDGES.add(new int[]{in.nextInt(), in.nextInt()});
-//            EDGES.add(new Pair(in.nextInt(), in.nextInt()));
-
+            POSITIVE_EDGES.add(new int[]{in.nextInt(), in.nextInt()});
         }
 
         RESULT.append(fragmentNumber)
                 .append(": ")
-                .append(executeCommands(molecules - 1))
+                .append(executeCommands(molecules))
                 .append('\n');
     }
 
     private static int executeCommands(int molecules) {
 
-        ArrayList<Triple> allTriples = EveryTriple.get(molecules);//getTriples(moleculeArray, new Triple(), 0, 0);
+        ArrayList<Triple> allTriples =
+//                EveryTriple.get(molecules);
+                getTriples(moleculeArray, new Triple(), 0, 0);
         int biggestGroupSize = 0;
         ArrayList<Triple> balancedTriples = new ArrayList<>();
-
 
         for (Triple triple : allTriples) {
             boolean balanced = isBalanced(triple);
@@ -67,8 +56,6 @@ public class Main {
         }
 
         ArrayList<Integer> group;
-//        ArrayList<HashSet<Integer>> badGroups = new ArrayList<>();
-
         for (Triple triple : balancedTriples) {
             group = new ArrayList<>();
             group.add(triple.left);
@@ -81,9 +68,9 @@ public class Main {
                 boolean skip = false;
 
                 // if the group doesn't contain the root node it's bad
-                if (!group.contains(1) && nextNumber != 1) {
+                //&& nextNumber > 1)
+                if (!group.contains(1))
                     break;
-                }
 
                 // Get a number that is not equal to the numbers currently in the triple
                 // Or the number is in the group less than there are combinations
@@ -94,23 +81,10 @@ public class Main {
                 if (skip) continue;
 
                 group.add(nextNumber);
-
-//                 If the group is already known for it's bad qualities it should not be selected
-//                for (HashSet<Integer> badGroup : badGroups) {
-//                    if (skip) break;
-//                    if (badGroup.containsAll(group) && group.containsAll(badGroup)) {
-//                        group.remove(group.size() - 1);
-//                        nextNumber++;
-//                        skip = true;
-//                    }
-//                }
-//                if (skip) continue;
-
                 ArrayList<Triple> newTriples = getTriples(group, new Triple(), 0, 0);
 
 
                 if (!balancedTriples.containsAll(newTriples)) {
-//                    badGroups.add(new HashSet<>(group));
                     group.remove(group.size() - 1);
                     nextNumber++;
                 }
@@ -128,31 +102,21 @@ public class Main {
      * @return true if the triple is balanced else false
      */
     private static boolean isBalanced(Triple triple) {
-//        Pair ab = new Pair(triple.get(0), triple.get(1));
-//        Pair ac = new Pair(triple.get(0), triple.get(2));
-//        Pair cb = new Pair(triple.get(2), triple.get(1));
-//
-//        return (EDGES.contains(ab) ^ EDGES.contains(ac) ^ EDGES.contains(cb)) ||
-//                (EDGES.contains(ab) && EDGES.contains(ac) && EDGES.contains(cb));
 
         int[] ab = new int[]{triple.get(0), triple.get(1)};
         int[] ac = new int[]{triple.get(0), triple.get(2)};
         int[] bc = new int[]{triple.get(2), triple.get(1)};
 
-        if (arrayContains(EDGES, ab) ^ arrayContains(EDGES, ac) ^ arrayContains(EDGES, bc))
-            return true;
-        else if (arrayContains(EDGES, ab) && arrayContains(EDGES, ac) && arrayContains(EDGES, bc))
-            return true;
-
-        else return false;
+        return (arrayContains(POSITIVE_EDGES, ab) ^ arrayContains(POSITIVE_EDGES, ac) ^ arrayContains(POSITIVE_EDGES, bc) ||
+                arrayContains(POSITIVE_EDGES, ab) && arrayContains(POSITIVE_EDGES, ac) && arrayContains(POSITIVE_EDGES, bc));
     }
 
-    private static boolean arrayContains(ArrayList<int[]> array, int[] value) {
+    private static boolean arrayContains(HashSet<int[]> array, int[] value) {
         boolean contains = false;
         for (int[] ints : array) {
             if (contains) break;
-            if (ints[0] == value[1] && ints[1] == value[0] ||
-                    ints[0] == value[0] && ints[1] == value[1])
+            if ((ints[0] == value[1] && ints[1] == value[0]) ||
+                    (ints[0] == value[0] && ints[1] == value[1]))
                 contains = true;
         }
         return contains;
@@ -203,37 +167,6 @@ public class Main {
         return permutations;
     }
 
-    final static class Pair {
-
-        final int left;
-        final int right;
-
-
-        Pair(int left, int right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Pair pair = (Pair) o;
-            return left == pair.left && right == pair.right ||
-                    left == pair.right && right == pair.left;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(left, right);
-        }
-
-        @Override
-        public String toString() {
-            return left + " - " + right;
-        }
-    }
-
     final static class Triple {
 
         final int left;
@@ -277,12 +210,15 @@ public class Main {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Triple triple = (Triple) o;
-            return left == triple.left && right == triple.right && middle == triple.middle ||
-                    left == triple.right && right == triple.left && middle == triple.middle ||
-                    left == triple.left && right == triple.middle && middle == triple.right ||
-                    left == triple.middle && right == triple.left && middle == triple.right ||
-                    left == triple.middle && right == triple.right && middle == triple.left ||
-                    left == triple.right && right == triple.middle && middle == triple.left;
+            return (left == triple.left && right == triple.right && middle == (triple.middle)) ||
+                    (left == (triple.right) && right == (triple.left) && middle == (triple.middle)) ||
+
+                    (left == (triple.left) && right == (triple.middle) && middle == (triple.right)) ||
+                    (left == (triple.middle) && right == (triple.left) && middle == (triple.right)) ||
+
+                    (left == (triple.middle) && right == (triple.right) && middle == (triple.left)) ||
+                    (left == (triple.right) && right == (triple.middle) && middle == (triple.left))
+                    ;
         }
 
         @Override
--- Problem CR/src/MainTest.java
+++ Problem CR/src/MainTest.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
